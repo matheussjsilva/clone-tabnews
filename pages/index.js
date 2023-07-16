04a6073aa1f493da8b8508910fbc5617de5048b2
Matheus Silva <109928058+matheussjsilva@users.noreply.github.com>
2023-07-16 23:01:47 +0000

    adiciona scripts `lint:check e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home() {
-    return <h1>Samila, você é uma pessoa especial.Se você também acha, dá uma risadinha! </h1>
+  return (
+    <h1>
+      Samila, você é uma pessoa especial.Se você também acha, dá uma risadinha!{" "}
+    </h1>
+  );
 }
 
 export default Home;
-
